4d5d08a23f60a5fd776bc14d4fb33141cc05e67f
srisaim249 <srisaim249@gmail.com>
2018-01-16 12:22:34 +0530

    tasks

---
+++ Day6/task_basedon_dictionary.py
@@ -0,0 +1,48 @@
+import sys
+import os.path
+import csv
+
+file_path=sys.argv[1]
+
+name=sys.argv[2]
+
+if not  os.path.isfile(file_path):
+	print("Sorry , file doesnt exists")
+	exit(12)
+
+file_pointer=open(file_path)
+
+csv_reader = csv.DictReader(file_pointer)
+
+
+
+list1=[]
+	
+count=0
+
+for line in csv_reader:
+	print(type(line))
+	if(line['first_name']==name):
+		if line['id'] not in list1:
+			count+=1
+			print line
+			print "the count is ",count
+
+	elif(line['last_name']==name):
+		if line['id'] not in list1:
+			count+=1
+			print line
+			print "the count is ",count
+
+	elif(line['id']==name):
+		if line['id'] not in list1:
+			count+=1
+			print line
+			print "the count is ",count
+
+	elif(line['email']==name):
+		if line['id'] not in list1:
+			count+=1
+			print line
+			print "the count is ",count
+	
---
+++ Day6/task_basedon_index.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import csv
+
+file_path=sys.argv[1]
+
+name=sys.argv[2]
+
+file_pointer=open(file_path,"r")
+
+if not os.path.isfile(file_path):
+	print("Sorry , file doesnt exists")
+	exit(12)
+
+
+lists=[]	
+record1=dict()
+count=0
+
+for line in file_pointer:	
+	#print(type(line))	
+	val=line.split(",")
+	user_id=val[0]
+	first_name = val[1]
+	last_name=val[2]
+	email=val[3]
+
+	if(first_name==name):
+		if user_id not in record1:
+			print user_id
+			count+=1
+			print "count is ",count
+			record1[user_id]=line
+			print record1[user_id]
+
+	elif(last_name==name):
+		if user_id not in record1:
+			print user_id
+			count+=1
+			print "count is ",count
+			record1[user_id]=line
+			print record1[user_id]
+
+
+	elif(email==name):
+		if user_id not in record1:
+			print user_id
+			count+=1
+			print "count is ",count
+			record1[user_id]=line
+			print record1[user_id]
+
+	elif(user_id==name):
+		count+=1
+		print line
+		if(count==1):
+			break
+			
+	
