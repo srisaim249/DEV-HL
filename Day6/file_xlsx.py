2956a0c078440acefaa47dda848a6af29b6321b9
srisaim249 <srisaim249@gmail.com>
2018-01-16 17:14:05 +0530

    fileinxlsx

---
+++ Day6/file_xlsx.py
@@ -0,0 +1,27 @@
+import xlrd
+import os.path
+
+
+
+file_path="/home/saisri/Desktop/file_for_xlsx.xlsx"
+
+book = xlrd.open_workbook(file_path,"rb")
+
+sheets=book.sheet_names()
+
+
+dictionary={}
+
+
+count=0
+for sheet_name in sheets:
+	sh=book.sheet_by_name(sheet_name)
+	for rownum in range(sh.nrows):
+		
+		row_values=sh.row_values(rownum)
+		val=row_values[0]
+
+		if val not in dictionary:
+			dictionary[val]=row_values
+
+print dictionary
