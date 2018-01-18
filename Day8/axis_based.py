5db1cec8b84210031e74124cc0c2af45aa860f34
srisaim249 <srisaim249@gmail.com>
2018-01-18 18:53:07 +0530

    comit

--- Day8/axis_based.py
+++ Day8/axis_based.py
@@ -13,13 +13,9 @@ col1=argument1.split(",")[1]
 row2=int(sys.argv[2])
 col2=int(sys.argv[3])
 
-if(col2<col1 or row2<row1):
-	print("please enter columns or columns properly properly")
-	exit(2345)
-
 row2+=1
 col2+=1
-
+s=" "
 
 file_path="/home/saisri/Desktop/clientdetails.xlsx"
 
@@ -30,9 +26,10 @@ sheets=book.sheet_by_index(0)
 
 
 for row in range(int(row1),int(row2)):
+	row_values=sheets.row_values(row)
 
 	for cols in range(int(col1),int(col2)):
-		row_values=sheets.row_values(row)
-
-		print row_values
-		
+		s=s+row_values[cols]+"		"
+	s=s+"\n"
+	
+print s	
